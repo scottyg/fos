41d3a769f3e933ab36b57e95dae879c3e6c11075
Scott Gordon <me@scottyg.net>
2014-07-10 14:32:37 -0700

    Finished web site

--- js/main.js
+++ js/main.js
@@ -1,16 +1,66 @@
 $(document).foundation();
 
 $(document).ready(function(){
+	$(".nav > a").click(function(){
+		$(".next").hide();
+		setTimeout(function(){
+			$(".next").fadeIn(1000);
+		}, 2000);
+	});
+	$(".about").click(function(){
+		$(".terminal-content").hide();
+		$("#terminal-about").show();
+		$(".info-content").hide();
+		$("#info-about").show();
+		$(".nav > a").removeClass("active");
+		$(this).addClass("active");
+	});
 	$(".install").click(function(){
-		$(".content").html("$ gem install fos");
-		$("#info-content").html("<h1>Installation</h1><p>Fos is a Ruby gem and can be easily installed from terminal with <span class='code'>$ gem install fos</span></p>");
+		$(".terminal-content").hide();
+		$("#terminal-install").show();
+		$(".info-content").hide();
+		$("#info-install").show();
+		$(".nav > a").removeClass("active");
+		$(this).addClass("active");
 	});
 	$(".use").click(function(){
-		$(".content").html("$ fos");
-		$("#info-content").html("<h1>Usage</h1><p>Fos archives <span class='code'>~/Desktop</span> and <span class='code'>~/Downloads</span> by default.<br/><br/>Try <span class='code'>$ fos -h</span> for more options.</p>");
+		$(".terminal-content").hide();
+		$("#terminal-use").show();
+		$(".info-content").hide();
+		$("#info-use").show();
+		$(".nav > a").removeClass("active");
+		$(this).addClass("active");
 	});
 	$(".dev").click(function(){
-		$(".content").html("$ bundle install<br/>$ rake install<br/>");
-		$("#info-content").html("<h1>Development</h1><p>More info coming soon</p>");
+		$(".terminal-content").hide();
+		$("#terminal-dev").show();
+		$(".info-content").hide();
+		$("#info-dev").show();
+		$(".nav > a").removeClass("active");
+		$(this).addClass("active");
 	});
+	$('a').click(function(){
+		$('html, body').animate({
+			scrollTop: $( $.attr(this, 'href') ).offset().top
+		}, 500);
+		return false;
+	});
+
+	setTimeout(function(){
+		$(".next").fadeIn(1000);
+	}, 2000);
+
+	
+	$(window).scroll(function(i){
+    	var scrollVar = $(window).scrollTop();
+    	console.log((500-scrollVar )/100);
+    	$('.fader').css({'opacity':( 100-scrollVar )/100});
+    	$('.fader').css({'top': .8*scrollVar });
+    	if(scrollVar>150){
+	    	$('.fader').hide();
+    	}
+    	if(scrollVar<150){
+	    	$('.fader').show();
+    	}
+	})
 });
